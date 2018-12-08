3d9b655835b5c35d541bec3aa1d8fc681cab850b
zsong11 <songmadrid11@gmail.com>
2018-12-08 15:00:41 -0800

    orgnized

---
+++ static/js/tabledata.js
@@ -0,0 +1,902 @@
+var data = [
+  {
+    rank: "1",
+    title: "Gone with the Wind",
+    studio: "MGM",
+    adjusted: "$1,846,687,900",
+    unadjusted: "$198,676,459",
+    year: "1939",
+    comments: ""
+  },
+  {
+    rank: "2",
+    title: "Star Wars",
+    studio: "FOX",
+    adjusted: "$1,628,013,100",
+    unadjusted: "$460,998,007",
+    year: "1977",
+    comments: ""
+  },
+  {
+    rank: "3",
+    title: "The Sound of Music",
+    studio: "FOX",
+    adjusted: "$1,301,676,500",
+    unadjusted: "$158,671,368",
+    year: "1965",
+    comments: ""
+  },
+  {
+    rank: "4",
+    title: "E.T.: The Extra-Terrestrial",
+    studio: "Uni.",
+    adjusted: "$1,296,548,600",
+    unadjusted: "$435,110,554",
+    year: "1982",
+    comments: ""
+  },
+  {
+    rank: "5",
+    title: "Titanic",
+    studio: "Par.",
+    adjusted: "$1,238,925,300",
+    unadjusted: "$659,363,944",
+    year: "1997",
+    comments: ""
+  },
+  {
+    rank: "6",
+    title: "The Ten Commandments",
+    studio: "Par.",
+    adjusted: "$1,197,340,000",
+    unadjusted: "$65,500,000",
+    year: "1956",
+    comments: ""
+  },
+  {
+    rank: "7",
+    title: "Jaws",
+    studio: "Uni.",
+    adjusted: "$1,170,640,400",
+    unadjusted: "$260,000,000",
+    year: "1975",
+    comments: ""
+  },
+  {
+    rank: "8",
+    title: "Doctor Zhivago",
+    studio: "MGM",
+    adjusted: "$1,134,598,100",
+    unadjusted: "$111,721,910",
+    year: "1965",
+    comments: ""
+  },
+  {
+    rank: "9",
+    title: "The Exorcist",
+    studio: "WB",
+    adjusted: "$1,010,876,400",
+    unadjusted: "$232,906,145",
+    year: "1973",
+    comments: ""
+  },
+  {
+    rank: "10",
+    title: "Snow White and the Seven Dwarfs",
+    studio: "Dis.",
+    adjusted: "$996,260,000",
+    unadjusted: "$184,925,486",
+    year: "1937",
+    comments: ""
+  },
+  {
+    rank: "11",
+    title: "Star Wars: The Force Awakens",
+    studio: "BV",
+    adjusted: "$988,172,000",
+    unadjusted: "$936,662,225",
+    year: "2015",
+    comments: ""
+  },
+  {
+    rank: "12",
+    title: "101 Dalmatians",
+    studio: "Dis.",
+    adjusted: "$913,243,700",
+    unadjusted: "$144,880,014",
+    year: "1961",
+    comments: ""
+    },
+  {
+    rank: "13",
+    title: "The Empire Strikes Back",
+    studio: "FOX",
+    adjusted: "$897,371,000",
+    unadjusted: "$290,475,067",
+    year: "1980",
+    comments: ""
+  },
+  {
+    rank: "14",
+    title: "Ben-Hur",
+    studio: "MGM",
+    adjusted: "$895,720,000",
+    unadjusted: "$74,000,000",
+    year: "1959",
+    comments: ""
+  },
+  {
+    rank: "15",
+    title: "Avatar",
+    studio: "FOX",
+    adjusted: "$889,409,500",
+    unadjusted: "$760,507,625",
+    year: "2009",
+    comments: ""
+  },
+  {
+    rank: "16",
+    title: "Return of the Jedi",
+    studio: "FOX",
+    adjusted: "$859,703,000",
+    unadjusted: "$309,306,177",
+    year: "1983",
+    comments: ""
+  },
+  {
+    rank: "17",
+    title: "Jurassic Park",
+    studio: "Uni.",
+    adjusted: "$837,423,500",
+    unadjusted: "$402,453,882",
+    year: "1993",
+    comments: ""
+  },
+  {
+    rank: "18",
+    title: "Star Wars: Episode I - The Phantom Menace",
+    studio: "FOX",
+    adjusted: "$825,452,400",
+    unadjusted: "$474,544,677",
+    year: "1999",
+    comments: ""
+  },
+  {
+    rank: "19",
+    title: "The Sting",
+    studio: "Uni.",
+    adjusted: "$814,765,700",
+    unadjusted: "$156,000,000",
+    year: "1973",
+    comments: ""
+  },
+  {
+    rank: "20",
+    title: "The Lion King",
+    studio: "BV",
+    adjusted: "$814,798,300",
+    unadjusted: "$422,783,777",
+    year: "1994",
+    comments: ""
+  },
+  {
+    rank: "21",
+    title: "Raiders of the Lost Ark",
+    studio: "Par.",
+    adjusted: "$809,287,100",
+    unadjusted: "$248,159,971",
+    year: "1981",
+    comments: ""
+  },
+  {
+    rank: "22",
+    title: "The Graduate",
+    studio: "AVCO",
+    adjusted: "$782,172,200",
+    unadjusted: "$104,945,305",
+    year: "1967",
+    comments: ""
+  },
+  {
+    rank: "23",
+    title: "Fantasia",
+    studio: "Dis.",
+    adjusted: "$759,017,400",
+    unadjusted: "$76,408,097",
+    year: "1941",
+    comments: ""
+  },
+  {
+    rank: "24",
+    title: "Jurassic World",
+    studio: "Uni.",
+    adjusted: "$722,509,700",
+    unadjusted: "$652,270,625",
+    year: "2015",
+    comments: ""
+  },
+  {
+    rank: "25",
+    title: "The Godfather",
+    studio: "Par.",
+    adjusted: "$721,352,300",
+    unadjusted: "$134,966,411",
+    year: "1972",
+    comments: ""
+  },
+  {
+    rank: "26",
+    title: "Forrest Gump",
+    studio: "Par.",
+    adjusted: "$718,537,700",
+    unadjusted: "$330,252,182",
+    year: "1994",
+    comments: ""
+  },
+  {
+    rank: "27",
+    title: "Mary Poppins",
+    studio: "Dis.",
+    adjusted: "$714,581,800",
+    unadjusted: "$102,272,727",
+    year: "1964",
+    comments: ""
+  },
+  {
+    rank: "28",
+    title: "Grease",
+    studio: "Par.",
+    adjusted: "$703,498,500",
+    unadjusted: "$188,755,690",
+    year: "1978",
+    comments: ""
+  },
+  {
+    rank: "29",
+    title: "Marvel's The Avengers",
+    studio: "BV",
+    adjusted: "$702,694,300",
+    unadjusted: "$623,357,910",
+    year: "2012",
+    comments: ""
+  },
+  {
+    rank: "30",
+    title: "Black Panther",
+    studio: "BV",
+    adjusted: "$700,059,566",
+    unadjusted: "NA",
+    year: "2012",
+    comments: ""
+  },
+  {
+    rank: "31",
+    title: "Thunderball",
+    studio: "UA",
+    adjusted: "$683,672,000",
+    unadjusted: "$63,595,658",
+    year: "1965",
+    comments: ""
+  },
+  {
+    rank: "32",
+    title: "The Dark Knight",
+    studio: "WB",
+    adjusted: "$680,596,400",
+    unadjusted: "$534,858,444",
+    year: "2008",
+    comments: ""
+  },
+  {
+    rank: "33",
+    title: "The Jungle Book",
+    studio: "Dis.",
+    adjusted: "$673,434,400",
+    unadjusted: "$141,843,612",
+    year: "1967",
+    comments: ""
+  },
+  {
+    rank: "34",
+    title: "Sleeping Beauty",
+    studio: "Dis.",
+    adjusted: "$664,259,200",
+    unadjusted: "$51,600,000",
+    year: "1959",
+    comments: ""
+  },
+  {
+    rank: "35",
+    title: "Avengers: Infinity War",
+    studio: "BV",
+    adjusted: "$678,815,482",
+    unadjusted: "NA",
+    year: "2018",
+    comments: ""
+  },
+  {
+    rank: "36",
+    title: "Ghostbusters",
+    studio: "Col.",
+    adjusted: "$650,527,900",
+    unadjusted: "$242,212,467",
+    year: "1984",
+    comments: ""
+  },
+  {
+    rank: "37",
+    title: "Shrek 2",
+    studio: "DW",
+    adjusted: "$649,405,500",
+    unadjusted: "$441,226,247",
+    year: "2004",
+    comments: ""
+  },
+  {
+    rank: "38",
+    title: "Butch Cassidy and the Sundance Kid",
+    studio: "FOX",
+    adjusted: "$644,898,800",
+    unadjusted: "$102,308,889",
+    year: "1969",
+    comments: ""
+  },
+  {
+    rank: "39",
+    title: "Love Story",
+    studio: "Par.",
+    adjusted: "$639,783,000",
+    unadjusted: "$106,397,186",
+    year: "1970",
+    comments: ""
+  },
+  {
+    rank: "40",
+    title: "Spider-Man",
+    studio: "Sony",
+    adjusted: "$635,090,600",
+    unadjusted: "$403,706,375",
+    year: "2002",
+    comments: ""
+  },
+  {
+    rank: "41",
+    title: "Independence Day",
+    studio: "FOX",
+    adjusted: "$633,117,500",
+    unadjusted: "$306,169,268",
+    year: "1996",
+    comments: ""
+  },
+  {
+    rank: "42",
+    title: "Home Alone",
+    studio: "FOX",
+    adjusted: "$619,090,600",
+    unadjusted: "$285,761,243",
+    year: "1990",
+    comments: ""
+  },
+  {
+    rank: "43",
+    title: "Star Wars: The Last Jedi",
+    studio: "BV",
+    adjusted: "$617,813,600",
+    unadjusted: "$620,181,382",
+    year: "2017",
+    comments: ""
+  },
+  {
+    rank: "44",
+    title: "Pinocchio",
+    studio: "Dis.",
+    adjusted: "$616,066,500",
+    unadjusted: "$84,254,167",
+    year: "1940",
+    comments: ""
+  },
+  {
+    rank: "45",
+    title: "Cleopatra",
+    studio: "FOX",
+    adjusted: "$614,056,800",
+    unadjusted: "$57,777,778",
+    year: "1963",
+    comments: ""
+  },
+  {
+    rank: "46",
+    title: "Beverly Hills Cop",
+    studio: "Par.",
+    adjusted: "$613,751,200",
+    unadjusted: "$234,760,478",
+    year: "1984",
+    comments: ""
+  },
+  {
+    rank: "47",
+    title: "Incredibles 2",
+    studio: "BV",
+    adjusted: "$608,553,983",
+    unadjusted: "NA",
+    year: "2018",
+    comments: ""
+  },
+  {
+    rank: "48",
+    title: "Goldfinger",
+    studio: "UA",
+    adjusted: "$605,982,000",
+    unadjusted: "$51,081,062",
+    year: "1964",
+    comments: ""
+  },
+  {
+    rank: "49",
+    title: "Airport",
+    studio: "Uni.",
+    adjusted: "$604,257,100",
+    unadjusted: "$100,489,151",
+    year: "1970",
+    comments: ""
+  },
+  {
+    rank: "50",
+    title: "American Graffiti",
+    studio: "Uni.",
+    adjusted: "$600,628,600",
+    unadjusted: "$115,000,000",
+    year: "1973",
+    comments: ""
+  },
+  {
+    rank: "51",
+    title: "The Robe",
+    studio: "FOX",
+    adjusted: "$598,254,500",
+    unadjusted: "$36,000,000",
+    year: "1953",
+    comments: ""
+  },
+  {
+    rank: "52",
+    title: "Pirates of the Caribbean: Dead Man's Chest",
+    studio: "BV",
+    adjusted: "$590,703,300",
+    unadjusted: "$423,315,812",
+    year: "2006",
+    comments: ""
+  },
+  {
+    rank: "53",
+    title: "Around the World in 80 Days",
+    studio: "UA",
+    adjusted: "$590,584,600",
+    unadjusted: "$42,000,000",
+    year: "1956",
+    comments: ""
+  },
+  {
+    rank: "54",
+    title: "Bambi",
+    studio: "RKO",
+    adjusted: "$582,331,800",
+    unadjusted: "$102,247,150",
+    year: "1942",
+    comments: ""
+  },
+  {
+    rank: "55",
+    title: "Blazing Saddles",
+    studio: "WB",
+    adjusted: "$578,010,200",
+    unadjusted: "$119,601,481",
+    year: "1974",
+    comments: ""
+  },
+  {
+    rank: "56",
+    title: "Batman",
+    studio: "WB",
+    adjusted: "$575,405,200",
+    unadjusted: "$251,188,924",
+    year: "1989",
+    comments: ""
+  },
+  {
+    rank: "57",
+    title: "The Bells of St. Mary's",
+    studio: "RKO",
+    adjusted: "$573,490,200",
+    unadjusted: "$21,333,333",
+    year: "1945",
+    comments: ""
+  },
+  {
+    rank: "58",
+    title: "The Lord of the Rings: The Return of the King",
+    studio: "NL",
+    adjusted: "$563,386,800",
+    unadjusted: "$377,845,905",
+    year: "2003",
+    comments: ""
+  },
+  {
+    rank: "59",
+    title: "Finding Nemo",
+    studio: "BV",
+    adjusted: "$562,900,700",
+    unadjusted: "$380,843,261",
+    year: "2003",
+    comments: ""
+  },
+  {
+    rank: "60",
+    title: "The Towering Inferno",
+    studio: "FOX",
+    adjusted: "$560,973,600",
+    unadjusted: "$116,000,000",
+    year: "1974",
+    comments: ""
+  },
+  {
+    rank: "61",
+    title: "Rogue One: A Star Wars Story",
+    studio: "BV",
+    adjusted: "$552,436,400",
+    unadjusted: "$532,177,324",
+    year: "2016",
+    comments: ""
+  },
+  {
+    rank: "62",
+    title: "Cinderella (1950)",
+    studio: "Dis.",
+    adjusted: "$551,155,100",
+    unadjusted: "$93,141,149",
+    year: "1950",
+    comments: ""
+  },
+  {
+    rank: "63",
+    title: "Spider-Man 2",
+    studio: "Sony",
+    adjusted: "$549,851,000",
+    unadjusted: "$373,585,825",
+    year: "2004",
+    comments: ""
+  },
+  {
+    rank: "64",
+    title: "My Fair Lady",
+    studio: "WB",
+    adjusted: "$548,400,000",
+    unadjusted: "$72,000,000 ",
+    year: "1964",
+    comments: ""
+  },
+  {
+    rank: "65",
+    title: "The Greatest Show on Earth",
+    studio: "Par.",
+    adjusted: "$548,400,000",
+    unadjusted: "$36,000,000",
+    year: "1952",
+    comments: ""
+  },
+  {
+    rank: "66",
+    title: "National Lampoon's Animal House",
+    studio: "Uni.",
+    adjusted: "$547,397,000",
+    unadjusted: "$141,600,000",
+    year: "1978",
+    comments: ""
+  },
+  {
+    rank: "67",
+    title: "The Passion of the Christ",
+    studio: "NM",
+    adjusted: "$545,702,300",
+    unadjusted: "$370,782,930",
+    year: "2004",
+    comments: ""
+  },
+  {
+    rank: "68",
+    title: "Star Wars: Episode III - Revenge of the Sith",
+    studio: "FOX",
+    adjusted: "$542,226,700",
+    unadjusted: "$380,270,577",
+    year: "2005",
+    comments: ""
+  },
+  {
+    rank: "69",
+    title: "Back to the Future",
+    studio: "Uni.",
+    adjusted: "$539,723,000",
+    unadjusted: "$210,609,762",
+    year: "1985",
+    comments: ""
+  },
+  {
+    rank: "70",
+    title: "The Lord of the Rings: The Two Towers",
+    studio: "NL",
+    adjusted: "$527,609,100",
+    unadjusted: "$342,551,365",
+    year: "2002",
+    comments: ""
+  },
+  {
+    rank: "71",
+    title: "The Dark Knight Rises",
+    studio: "WB",
+    adjusted: "$526,297,700",
+    unadjusted: "$448,139,099",
+    year: "4012",
+    comments: ""
+  },
+  {
+    rank: "72",
+    title: "The Sixth Sense",
+    studio: "BV",
+    adjusted: "$526,273,200",
+    unadjusted: "$293,506,292",
+    year: "1999",
+    comments: ""
+  },
+  {
+    rank: "73",
+    title: "Superman",
+    studio: "WB",
+    adjusted: "$524,425,500",
+    unadjusted: "$134,384,367",
+    year: "1978",
+    comments: ""
+  },
+  {
+    rank: "74",
+    title: "Tootsie",
+    studio: "Col.",
+    adjusted: "$520,102,00",
+    unadjusted: "$177,200,000",
+    year: "1982",
+    comments: ""
+  },
+  {
+    rank: "75",
+    title: "Smokey and the Bandit",
+    studio: "Uni.",
+    adjusted: "$519,453,000",
+    unadjusted: "$126,737,428",
+    year: "1977",
+    comments: ""
+  },
+  {
+    rank: "76",
+    title: "Beauty and the Beast",
+    studio: "BV",
+    adjusted: "$519,135,600",
+    unadjusted: "$504,014,165",
+    year: "2017",
+    comments: ""
+  },
+  {
+    rank: "77",
+    title: "Finding Dory",
+    studio: "BV",
+    adjusted: "$513,284,900",
+    unadjusted: "$486,295,561",
+    year: "2016",
+    comments: ""
+  },
+  {
+    rank: "78",
+    title: "West Side Story",
+    studio: "MGM",
+    adjusted: "$511,966,100",
+    unadjusted: "$44,064,958",
+    year: "1961",
+    comments: ""
+  },
+  {
+    rank: "79",
+    title: "Close Encounters of the Third Kind",
+    studio: "Col.",
+    adjusted: "$511,133,900",
+    unadjusted: "$135,189,114",
+    year: "1977",
+    comments: ""
+  },
+  {
+    rank: "80",
+    title: "Harry Potter and the Sorcerer's Stone",
+    studio: "WB",
+    adjusted: "$511,044,600",
+    unadjusted: "$317,575,550",
+    year: "2001",
+    comments: ""
+  },
+  {
+    rank: "81",
+    title: "Lady and the Tramp",
+    studio: "Dis.",
+    adjusted: "$509,416,800",
+    unadjusted: "$93,602,326",
+    year: "1955",
+    comments: ""
+  },
+  {
+    rank: "82",
+    title: "Lawrence of Arabia",
+    studio: "Col.",
+    adjusted: "$506,205,700",
+    unadjusted: "$44,824,144",
+    year: "1962",
+    comments: ""
+  },
+  {
+    rank: "83",
+    title: "The Rocky Horror Picture Show",
+    studio: "FOX",
+    adjusted: "$503,334,500",
+    unadjusted: "$112,892,319",
+    year: "1975",
+    comments: ""
+  },
+  {
+    rank: "84",
+    title: "Rocky",
+    studio: "UA",
+    adjusted: "$503,065,400",
+    unadjusted: "$117,235,147",
+    year: "1976",
+    comments: ""
+  },
+  {
+    rank: "85",
+    title: "The Best Years of Our Lives",
+    studio: "RKO",
+    adjusted: "$502,700,000",
+    unadjusted: "$23,650,000",
+    year: "1946",
+    comments: ""
+  },
+  {
+    rank: "86",
+    title: "The Poseidon Adventure",
+    studio: "FOX",
+    adjusted: "$501,803,900",
+    unadjusted: "$84,563,118",
+    year: "1972",
+    comments: ""
+  },
+  {
+    rank: "87",
+    title: "he Lord of the Rings: The Fellowship of the Ring",
+    studio: "NL",
+    adjusted: "$500,865,400",
+    unadjusted: "$315,544,750",
+    year: "2001",
+    comments: ""
+  },
+  {
+    rank: "88",
+    title: "Twister",
+    studio: "WB",
+    adjusted: "$499,849,500",
+    unadjusted: "$241,721,524",
+    year: "1996",
+    comments: ""
+  },
+  {
+    rank: "89",
+    title: "Men in Black",
+    studio: "Sony",
+    adjusted: "$499,196,400",
+    unadjusted: "$250,690,539",
+    year: "1997",
+    comments: ""
+  },
+  {
+    rank: "90",
+    title: "The Bridge on the River Kwai",
+    studio: "Col.",
+    adjusted: "$497,216,000",
+    unadjusted: "$27,200,000",
+    year: "1957",
+    comments: ""
+  },
+  {
+    rank: "91",
+    title: "Transformers: Revenge of the Fallen",
+    studio: "P/DW",
+    adjusted: "$492,654,500",
+    unadjusted: "$402,111,870",
+    year: "2009",
+    comments: ""
+  },
+  {
+    rank: "92",
+    title: "It's a Mad, Mad, Mad, Mad World",
+    studio: "MGM",
+    adjusted: "$492,421,300",
+    unadjusted: "$46,332,858",
+    year: "1963",
+    comments: ""
+  },
+  {
+    rank: "93",
+    title: "Swiss Family Robinson",
+    studio: "Dis.",
+    adjusted: "Dis.",
+    unadjusted: "$40,356,000",
+    year: "1960",
+    comments: ""
+  },
+  {
+    rank: "94",
+    title: "One Flew Over the Cuckoo's Nest",
+    studio: "UA",
+    adjusted: "$490,684,200",
+    unadjusted: "$108,981,275",
+    year: "1975",
+    comments: ""
+  },
+  {
+    rank: "95",
+    title: "M.A.S.H.",
+    studio: "FOX",
+    adjusted: "$490,673,700",
+    unadjusted: "$81,600,000",
+    year: "1970",
+    comments: ""
+  },
+  {
+    rank: "96",
+    title: "Indiana Jones and the Temple of Doom",
+    studio: "Par.",
+    adjusted: "$489,289,900",
+    unadjusted: "$179,870,271",
+    year: "1984",
+    comments: ""
+  },
+  {
+    rank: "97",
+    title: "Avengers: Age of Ultron",
+    studio: "BV",
+    adjusted: "$489,236,000",
+    unadjusted: "$459,005,868",
+    year: "2015",
+    comments: ""
+  },
+  {
+    rank: "98",
+    title: "Star Wars: Episode II - Attack of the Clones",
+    studio: "FOX",
+    adjusted: "$488,701,800",
+    unadjusted: "$310,676,740",
+    year: "2002",
+    comments: ""
+  },
+  {
+    rank: "99",
+    title: "Toy Story 3",
+    studio: "BV",
+    adjusted: "$487,522,400",
+    unadjusted: "$415,004,880",
+    year: "2010",
+    comments: ""
+  },
+  {
+    rank: "100",
+    title: "Mrs. Doubtfire",
+    studio: "FOX",
+    adjusted: "$481,535,300",
+    unadjusted: "$219,195,243",
+    year: "1993",
+    comments: ""
+  }
+];
