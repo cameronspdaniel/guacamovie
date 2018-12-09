97b3b4470efceb28a1a1c742ef4e532e951fdbfb
C Daniel <cameronspdaniel@gmail.com>
2018-12-08 16:04:20 -0800

    reverted app.py

--- app.py
+++ app.py
@@ -18,21 +18,5 @@ def moviemap():
 def boxoffice():
     return render_template("boxoffice.html")
 
-conn = "mongodb://localhost:41108"
-client = pymongo.MongoClient(conn)
-
-# connect to mongo db and collection
-db = client.heroku_xwwjj6bv
-collection = db.KaggleMovies
-
-@app.route("/movie")
-def movie():
-    # write a statement that finds all the items in the db and sets it to a variable
-    films = list(db.collection.find())
-    print(films)
-
-    # render an index.html template and pass it the data you retrieved from the database
-    return render_template("movie.html", films=films)
-
 if __name__ == "__main__":
     app.run()
