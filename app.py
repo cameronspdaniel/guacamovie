e0f23af61616ab0d5c076e252e8a3379b71bafbb
zsong11 <songmadrid11@gmail.com>
2018-12-09 21:47:12 -0800

    genre added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,25 @@ def moviemap():
 @app.route("/boxoffice")
 def boxoffice():
     return render_template("boxoffice.html")
+
+@app.route('/genre')
+def index():
+    return render_template('genre.html')
+
+@app.route('/movies')
+def movies():
+    movies = dr.get_movies()
+    result = []
+    for i,movie in enumerate(movies):
+        if i == 0:
+            continue
+        result.append({
+            'gross': movie[0],
+            'year': movie[1],
+            'genre': movie[2]
+        })
+    return jsonify(result)
+
 
 
 
