16af6d0f92fcebe7d2bfc27d1d38bfc9ac6c3f63
Prabh Ratoul <Prabhdeep@Prabh-Ratouls-MacBook-Pro.local>
2018-12-09 18:11:01 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import pandas as pd
+import data_retrieval as dr
+
+from flask import Flask, jsonify, render_template, request
 
 import pymongo
 
@@ -17,6 +20,27 @@ def moviemap():
 @app.route("/boxoffice")
 def boxoffice():
     return render_template("boxoffice.html")
+
+@app.route('/budget')
+def index():
+    return render_template('budget.html')
+
+@app.route('/movies')
+def movies():
+    movies = dr.get_movies()
+    result = []
+    for i,movie in enumerate(movies):
+        if i == 0:
+            continue
+        result.append({
+            'budget': movie[0],
+            'revenue': movie[1],
+            'name': movie[2],
+            'runtime': movie[3],
+            'year': movie[4]
+        })
+    return jsonify(result)   
+
 
 if __name__ == "__main__":
     app.run()
