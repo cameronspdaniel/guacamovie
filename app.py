43325d0818126f9e8847bd6ccc1a87c82c851d92
C Daniel <cameronspdaniel@gmail.com>
2018-12-09 23:53:28 -0800

    consolidated everything into one python script

--- app.py
+++ app.py
@@ -1,3 +1,55 @@
+# coding: utf-8
+
+# In[1]:
+
+from db import engine, Movies
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func, extract
+
+
+# In[2]:
+
+
+Session = sessionmaker(bind=engine)
+
+
+def get_movies():
+    return Session().query(Movies.Budget, Movies.Gross, Movies.Name, Movies.Runtime, Movies.Year).all()
+
+
+# In[1]:
+
+
+from sqlalchemy import create_engine, Table, ForeignKey, Column, Boolean, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.pool import StaticPool
+
+
+# In[2]:
+
+
+engine = create_engine('sqlite:///movies.sqlite', connect_args={'check_same_thread':False}, poolclass=StaticPool)
+Base = declarative_base()
+
+
+
+class Movies(Base):
+    __tablename__ = 'Movies'
+    
+    Id = Column(Integer, primary_key=True)
+    Budget = Column(Integer)
+    Genre = Column(String(20))
+    Gross = Column(Integer)
+    Name = Column(String(100))
+    Runtime = Column(Integer)
+    Year = Column(Integer)
+    
+# In[9]:
+
+
+Base.metadata.create_all(engine)
+
 import pandas as pd
 import data_retrieval as dr
 
