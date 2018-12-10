182291b8d1ab3eeae281a4375c2cb9bd4b0c6624
zsong11 <songmadrid11@gmail.com>
2018-12-09 23:15:52 -0800

    round 2

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import pandas as pd
-import data_retrieval as dr
-
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -18,44 +15,6 @@ def moviemap():
 @app.route("/boxoffice")
 def boxoffice():
     return render_template("boxoffice.html")
-
-@app.route('/genre')
-def genre():
-    return render_template('genre.html')
-
-@app.route('/movies')
-def movies():
-    movies = dr.get_movies()
-    result = []
-    for i,movie in enumerate(movies):
-        if i == 0:
-            continue
-        result.append({
-            'gross': movie[0],
-            'year': movie[1],
-            'genre': movie[2]
-        })
-    return jsonify(result)
-
-#@app.route('/budget')
-#def budget():
-#    return render_template('budget.html')
-
-#@app.route('/movies')
-#def movies():
-#    movies = dr.get_movies()
-##    result = []
-#    for i,movie in enumerate(movies):
-#        if i == 0:
-#            continue
-#        result.append({
-#            'budget': movie[0],
-###         'name': movie[2],
-#            'runtime': movie[3],
-#            'year': movie[4]
-#        })
-#    return jsonify(result)
-
 
 
 
