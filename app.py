684d0ecc7b1da60a33c405f1877e24775b96a71b
C Daniel <cameronspdaniel@gmail.com>
2018-12-09 23:26:32 -0800

    adding all of prabh's files in the correct places

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import pandas as pd
+import data_retrieval as dr
+
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -16,8 +19,25 @@ def moviemap():
 def boxoffice():
     return render_template("boxoffice.html")
 
+@app.route('/budget')
+def budget():
+    return render_template('budget.html')
 
-
+@app.route('/movies')
+def movies():
+    movies = dr.get_movies()
+    result = []
+    for i,movie in enumerate(movies):
+        if i == 0:
+            continue
+        result.append({
+            'budget': movie[0],
+            'revenue': movie[1],
+            'name': movie[2],
+            'runtime': movie[3],
+            'year': movie[4]
+        })
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run()
